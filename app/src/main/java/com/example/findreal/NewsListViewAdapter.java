a191d84382e4bb32f1acbd7e81b3c959c7fe112a
Woojin <hyung12379@gmail.com>
2020-08-06 00:54:29 +0900

    NewsListView custom and Request Box layout

--- app/src/main/java/com/example/findreal/NewsListViewAdapter.java
+++ app/src/main/java/com/example/findreal/NewsListViewAdapter.java
@@ -4,48 +4,61 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 
-public class NewsListViewAdapter {
+public class NewsListViewAdapter extends RecyclerView.Adapter<NewsListViewAdapter.NewsListViewHolder>{
     private ArrayList<NewsListViewItem> newsListViewItems = new ArrayList<NewsListViewItem>();
+
+    class NewsListViewHolder extends RecyclerView.ViewHolder {
+        private ImageView thumbnail;
+        private TextView title;
+
+        NewsListViewHolder(View itemView) {
+            super(itemView);
+
+            thumbnail = itemView.findViewById(R.id.news_thumbnail);
+            title = itemView.findViewById(R.id.news_title);
+        }
+
+        void onBind(NewsListViewItem item) {
+            thumbnail.setImageDrawable(item.getThumbnail());
+            title.setText(item.getNewsTitle());
+
+        }
+    }
 
     public NewsListViewAdapter() {
 
     }
 
-    //@Override
-    public int getCount() {
-        return newsListViewItems.size();
+    @NonNull
+    @Override
+    public NewsListViewAdapter.NewsListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_listview_item, parent, false);
+        return new NewsListViewHolder(view);
     }
 
-    //@Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final int pos = position;
-        final Context context = parent.getContext();
-
-        if (convertView == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.news_listview_item, parent, false);
-        }
-
-        ImageView thumbnailImageView = (ImageView) convertView.findViewById(R.id.news_thumbnail);
-        TextView titleTextView = (TextView) convertView.findViewById(R.id.news_title);
-
-        NewsListViewItem newsListViewItem = newsListViewItems.get(position);
-
-        thumbnailImageView.setImageDrawable(newsListViewItem.getThumbnail());
-        titleTextView.setText(newsListViewItem.getNewsTitle());
-
-        return convertView;
+    @Override
+    public void onBindViewHolder(@NonNull NewsListViewAdapter.NewsListViewHolder holder, int position) {
+        holder.onBind(newsListViewItems.get(position));
     }
 
-    //@Override
+
+    @Override
     public long getItemId(int position) {
         return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return newsListViewItems.size();
     }
 
     //@Override
